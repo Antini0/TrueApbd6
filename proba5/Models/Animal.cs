d2569628e9940b51ba387f61eb733d52bb510473
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-04-19 13:23:40 +0200

    MOD initial

---
+++ proba5/Models/Animal.cs
@@ -0,0 +1,7 @@
+﻿namespace proba5.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string name { get; set; }
+}
---
+++ proba5/Models/DTOs/AddAnimal.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace proba5.Models.DTOs;
+
+public class AddAnimal
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(200)]
+    public string name { get; set; }
+    [MaxLength(200)]
+    public string? description { get; set; }
+}
